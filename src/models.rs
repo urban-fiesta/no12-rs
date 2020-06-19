a44c0a83a444aeff0fca9a12709bf0a3b69747fa
Caio Raposo <caioraposo@protonmail.com>
2020-06-19 13:51:43 -0300

    Remove comments

--- src/models.rs
+++ src/models.rs
@@ -38,31 +38,3 @@ pub type Db = Arc<Mutex<Vec<Event>>>;
 pub fn blank_db() -> Db {
     Arc::new(Mutex::new(Vec::new()))
 }
-
-// impl User {
-//     pub fn new(id: u64, name: String, email: String, password: String) -> Self {
-//         User {
-//             id,
-//             name,
-//             email,
-//             password,
-//         }
-//     }
-// }
-
-// impl Event {
-//     pub fn new(id: u64, name: String, date: String, local: String, start: String, finish: String, price: f32, description: String, age_limit: u32, event_photo: String) -> Self {
-//         Event {
-//             id,
-//             name,
-//             date,
-//             local,
-//             start,
-//             finish,
-//             price,
-//             description,
-//             age_limit,
-//             event_photo,
-//         }
-//     }
-// }
